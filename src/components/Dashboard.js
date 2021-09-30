5d25d35e27b7c68263a5dd98c4a4c3bc9b110c87
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-30 18:32:09 +0530

    get data from API

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-export const Dashboard = () =>{
-    return(
-        <div>
+export const Dashboard = () => {
 
-        </div>
+    const url = 'https://www.hpb.health.gov.lk/api/get-current-statistical'
+
+    const [covidData, setCovidData] = useState(null)
+
+    useEffect(() => {
+
+        axios.get(url).
+            then(response => {
+
+                setCovidData(response.data)
+
+            })
+
+    }, [url])
+
+
+    if (covidData) {
+        return (
+            <>
+
+
+                <h2>Covid Status Dashboard</h2>
+                
+
+
+
+
+
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div>
+                <h2>Data Not Found</h2>
+            </div>
+        </>
     )
+
 }
