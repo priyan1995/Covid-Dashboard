6fd6b39613f73781cbfe48997969fd75f58bbb38
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-30 23:15:51 +0530

    UI devleoped

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -21,42 +21,97 @@ export const Dashboard = () => {
         return (
             <>
 
+
+
                 <div className="container">
-                    <div className="row">                       
+                    <div className="figr-box">
+                        <div className="row">
 
-                        <div className="col-lg-4">
-                            <div className="boxed">
-                                <h4>Total Confirmed Cases</h4>
-                                <p>{covidData.local_total_cases}</p>
+                            <div className="title-sec"><h3>Total Figures (SL)</h3></div>
+
+                            <div className="seprator"></div>
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Confirmed Cases</h4>
+                                        <p className="txt-warn">{covidData.local_total_cases}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Recovered</h4>
+                                        <p className="txt-succ">{covidData.local_recovered}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Deaths</h4>
+                                        <p className="txt-dngr">{covidData.local_deaths}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12">
+                                <p className="upd-dt"> Last updated at: {covidData.update_date_time}</p>
                             </div>
                         </div>
-
-
-                        <div className="col-lg-4">
-                            <div className="boxed">
-                                <h4>Total Recovered</h4>
-                                <p>{covidData.local_recovered}</p>
-                            </div>
-                        </div>
-
-
-                        <div className="col-lg-4">
-                            <div className="boxed">
-                                <h4>Total Deaths</h4>
-                                <p>{covidData.local_deaths}</p>
-                            </div>
-                        </div>
-
-
-
-
-
                     </div>
+
+
+                    <div className="figr-box">
+                        <div className="row">
+
+                            <div className="title-sec"><h3>Daily Figures (SL)</h3></div>
+
+                            <div className="seprator"></div>
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Confirmed Cases</h4>
+                                        <p className="txt-warn">{covidData.local_total_cases}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Recovered</h4>
+                                        <p className="txt-succ">{covidData.local_recovered}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div className="col-lg-4">
+                                <div className="boxed">
+                                    <div>
+                                        <h4>Total Deaths</h4>
+                                        <p className="txt-dngr">{covidData.local_deaths}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12">
+                                <p className="upd-dt"> Last updated at: {covidData.update_date_time}</p>
+                            </div>
+
+                            
+                        </div>
+                    </div>
+
+
+
                 </div>
-
-
-
-
 
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,12 @@
 
 export const Header = () => {
     return (
-        <div className="col-lg-12">
-            <h2>Covid Status Dashboard</h2>
-        </div>
+        <>
+            <div className="pg-header">
+                <div className="col-lg-12">
+                    <h2><strong>COVID-19</strong> : Live Situational Analysis Dashboard of Sri Lanka</h2>
+                </div>
+            </div>
+        </>
     )
 }
